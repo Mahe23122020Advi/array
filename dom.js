5fc07dda37e764a6bd41c686e1384d52c32513ef
Mahe23122020Advi <maheaadvik@gmail.com>
2024-02-07 11:41:42 +0530

    js commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+ document.getElementById("hi").innerHTML= "Hello world"
+
+ document.getElementById("good").innerHTML="goodbye"
+
+ document.getElementById("bad").innerHTML="bye bye"
+
+ let input=document.getElementById("input").value;
+console.log(input);
---
+++ index.js
@@ -0,0 +1,149 @@
+//  key variable = data (or value )
+
+//    var let const
+
+//    var num1 = 12
+     
+//    var num1 = 34
+
+//    let num2 = 36
+
+//    let num2 = 38
+
+// const num3 = 45
+// //  const num3 = 65
+// console.log(num3);
+
+var number2 = 10
+var number2 = 15
+console.log(number2);
+
+let user ="vaani" 
+let user1 ="raani"
+console.log(user);
+
+const user5 ="mahe"
+const user3 ="vijay"
+console.log(user5)
+//scope -> globle scope , functional scope , block scope 
+//assignement operator += -= *=
+//comparison operator <> == ===
+//logical operator && || !
+
+let mark=50;
+let tc=100;
+let x = mark < tc;
+console.log(x);
+
+let markk=false;
+let tcc=false;
+admission= markk||tcc;
+console.log(!admission);
+
+let mar=false;
+let t=false;
+admission= mar||t;
+console.log(admission);
+
+//ternary operator ? : ;
+let tcp="10";
+tcp==10 ? console.log("yes"):console.log("no");
+
+let tccp="10";
+tccp===10 ? console.log("yes"):console.log("no");
+
+
+    
+///if else condition is true or false////////
+
+if(tccp ==10)
+{
+console.log("true");
+}
+else
+{
+    console.log("false");
+}
+
+/////////////switch case the condition is true ah irundhaal this display//////
+let a =10
+let b =11
+switch (true) {
+    case a < b:
+        console.log("yes this is monday");
+    break;
+        // case "tuesday":
+        // console.log("yes this is tuesday");
+        // break;
+        // case "wednesday":
+        // console.log("yes this is  wednesday");
+        // break;
+    default:
+        console.log("not found ");
+    break;
+}
+
+
+
+function first (){
+
+    fun ()
+
+
+}
+first ()
+
+
+function fun (){
+    let data =23
+    console.log(data);
+    
+
+}
+
+let second =()=>{
+    console.log("second");
+}
+
+
+
+
+
+let array = [12,12.5,"friday",true]
+
+let array1 = [1,2,3,4,5]
+
+
+// index or position 
+   
+
+   array.pop()
+   array.push("danial")
+
+   array.shift()
+
+   array.unshift("makesh")
+   console.log(array.length);
+
+
+    // spread operator 
+    let array_value = [...array,...array1]
+
+    console.log(array_value.length);
+
+    for( i= 0 ; i<array_value.length ; i++){
+    console.log(array_value[i]);
+
+     }  
+    
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+
